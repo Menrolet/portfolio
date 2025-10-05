c997a9bf0922f077b07e1bb932287f3db2493c7f
Menrolet <suhaldos@gmail.com>
2025-10-05 20:59:41 +0300

    2.3

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const skills = {
+  data: [
+    { name: "html", level: 60, icon: "html.svg" },
+    { name: "css", level: 70, icon: "css.svg" },
+    { name: "python", level: 80, icon: "python.svg" },
+    { name: "c++", level: 55, icon: "c++.svg" }
+  ],
+
+  generateList(parentElement) {
+    parentElement.innerHTML = "";
+
+    this.data.forEach(skill => {
+      const dt = document.createElement("dt");
+      dt.classList.add("skill-item");
+      dt.textContent = skill.name;
+      dt.style.backgroundImage = `url(../img/${skill.icon})`;
+
+      const dd = document.createElement("dd");
+      dd.classList.add("skill-level");
+
+      const div = document.createElement("div");
+      div.style.width = `${skill.level}%`;
+      div.textContent = `${skill.level}%`;
+
+      dd.appendChild(div);
+      parentElement.appendChild(dt);
+      parentElement.appendChild(dd);
+    });
+  }
+};
+
+const skillList = document.querySelector("dl.skill-list");
+skills.generateList(skillList);
